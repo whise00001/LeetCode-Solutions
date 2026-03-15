1d74b3bd30cce50b430e942797397ebd10a313db
whise <tujinwei471@gmail.com>
2026-03-15 14:14:11 +0800

    L136 update

--- Easy/L_136_singleNumber.py
+++ Easy/L_136_singleNumber.py
@@ -1,3 +1,29 @@
+'''
+初步想法:
+    想到hashmap紀錄每個數出現的次數
+    Time complexity:O(n)
+    Space complexity:O(n)
+進階:
+    可以使用XOR的特性兩兩消除
+    Time complexity:O(n)
+    Space complexity:O(1)
+'''
+
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         hashmap = {}
+#         for i in nums:
+#             hashmap[i] = hashmap.get(i,0) + 1
+
+#         for i in hashmap:
+#             if hashmap[i] == 1:
+#                 return i          
+   
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
+        res = 0
+
+        for n in nums:
+            res ^= n
+
+        return res 
